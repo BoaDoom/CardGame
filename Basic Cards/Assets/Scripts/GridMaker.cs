f90f44fad511c6e721d6a5aa27fb57c8d9c6e6a6
BoaDoom <BoaDoom@gmail.com>
2017-03-08 14:37:30 -0800

    raycast in gridmaker is buggy

--- Basic Cards/Assets/Scripts/GridMaker.cs
+++ Basic Cards/Assets/Scripts/GridMaker.cs
@@ -35,10 +35,19 @@ public class GridMaker : MonoBehaviour {
 	Vector3 framingBoxSize;
 	Vector3 firstBoxCord;
 
-	RaycastHit rayHit;
-	Ray ray;
-
+	public RaycastHit2D hit;
+	public Ray ray;
+	public Vector3 rayPosition;
 	//public bool mouseOnPlayArea;
+	public float mouseXpos;
+	public float mouseYpos;
+	public Vector3 junkV3;
+	public Vector2 origin;
+
+	public Transform sphere;
+	public Transform cube;
+
+	RaycastHit2D[] hitInfo;
 
 	void Start () {
 		//mouseOnPlayArea = false;
@@ -56,7 +65,7 @@ public class GridMaker : MonoBehaviour {
 			smallSquareInst = Instantiate (smallSquare, zeroCord, transformOriginal.rotation);
 			smallSquareInst.transform.SetParent (gameObject.transform);
 			smallSquareInst.transform.localScale = framingBoxSize * sizeRatioOfSmallBox;
-			smallSquareInst.transform.localPosition = firstBoxCord + new Vector3(framingBoxSize.x*xi, -framingBoxSize.y*yi, 0.0f);
+			smallSquareInst.transform.localPosition = firstBoxCord + new Vector3(framingBoxSize.x*xi, -framingBoxSize.y*yi, -0.1f);
 			if (xi >= boxCountX-1) {
 				xi = 0;
 				yi++;
@@ -84,14 +93,60 @@ public class GridMaker : MonoBehaviour {
 //		Debug.Log ("gridmaker mouseonplayarea false");
 //	}
 	void Update () {
-			RaycastHit hit;
-			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-			if (Physics.Raycast (ray, out hit)) {
-				if (hit.collider.tag == "Card") {
-					hit.collider.enabled = false;
-					Debug.Log ("raycasted");
-				}
+		
+		if (Input.GetMouseButtonDown(0))
+		{
+			float rayDistance = 20.0f;
+			Vector3 mouseWorldPosition = Camera.main.transform.position;
+			Vector3 screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+			Vector3 mouseWorldPosition2 = Camera.main.ScreenToWorldPoint(new Vector3((Input.mousePosition).x, (Input.mousePosition).y, screenPoint.z));
+			Ray ray = new Ray (mouseWorldPosition2, Vector3.zero);
+				//Debug.Log ("cast");
+			hitInfo = Physics2D.GetRayIntersectionAll(ray, rayDistance);
+			Debug.Log (hitInfo.Length);
+			for(int i =0; hitInfo.Length > i; i++){
+				Debug.Log (hitInfo[i].collider.gameObject.name);
+			}
+			
+			//origin = new Vector2(junkV3.x, junkV3.y);
+			//if (Physics2D.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), Vector2.zero)){
+			//Debug.Log(hitInfo.transform.name);
+			//}
+
+
+			sphere.position = mouseWorldPosition;
+			cube.position = mouseWorldPosition2;
 		}
+//		mouseXpos = Input.mousePosition.x;
+//		mouseYpos = Input.mousePosition.y;
+//		mouseXposAC = (new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0.0f));
+//		rayPosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane));
+//		//rayPosition = new Vector3 ((Input.mousePosition).x, (Input.mousePosition).y, 0.0f);
+//		ray = new Ray (rayPosition, Vector3.zero);
+//		hit = Physics2D.GetRayIntersection(ray, 0.0f);
+//		if (hit) {
+//			Debug.Log (hit.transform.name);
+//		}
+
+//		mouseXpos = Input.mousePosition.x;
+//		mouseYpos = Input.mousePosition.y;
+//		mouseXposAC = (new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0.0f));
+//		mouseYposAC = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0.0f));
+//		rayPosition = new Vector3 ((Input.mousePosition).x, (Input.mousePosition).y, 0.0f);
+//		ray = new Ray (rayPosition, new Vector3(5.0f, 5.0f, 5.0f));
+//		hit = Physics2D.GetRayIntersection(ray, 20.0f);
+//		if (hit) {
+//			Debug.Log (hit.transform.name);
+//		}
+
+//		//ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+//		if (Physics2D.Raycast (ray, out hit)) {
+//			if (hit.transform.tag == "Card") {
+//				//hit.transform. = false;
+//				Debug.Log ("raycasted");
+//			}
+//			Debug.Log (hit.transform.tag);
+//		}
 //		if 
 //		ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 //			RaycastHit hit2;
