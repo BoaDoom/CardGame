e2d1f5242eb81591d64cfe6ebd21501a80210bec
BoaDoom <BoaDoom@gmail.com>
2017-03-09 13:46:44 -0800

    squares are in a matrix but arn't taking labels

---
+++ Basic Cards/Assets/Scripts/ActiveSquareBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActiveSquareBehaviour : MonoBehaviour {
+
+	int gridCordX;
+	int gridCordY;
+
+	public int GridCordX{
+		get{return gridCordX;}
+		set{gridCordX = GridCordX;}
+	}
+	public int GridCordY{
+		get{return gridCordY;}
+		set{gridCordX = GridCordY;}
+	}
+}
--- Basic Cards/Assets/Scripts/GridMaker.cs
+++ Basic Cards/Assets/Scripts/GridMaker.cs
@@ -4,22 +4,7 @@ using UnityEngine;
 
 public class GridMaker : MonoBehaviour {
 
-//	public int gridWidthInBoxes;
-//	public int gridHeightInBoxes;
-//	public Transform smallSquare;
-//
-//	public List<Transform> gridList;
-//
-//	Transform smallGridSize;
-//	Transform transformOriginal;
-//
-//	Vector3 zeroCord;
-//	Vector3 smallSquareOutterGap;
-//	public int ratioOfGap;
-//	public int actualScaleOfPlayArea;
-//	Vector3 smallSquareScale;
-
-	public Transform smallSquare;
+	public ActiveSquareBehaviour smallSquare;
 	Transform transformOriginal;
 	public Transform playAreaDetector;
 	public PlayAreaDetectorScript playAreaDetectorScript;
@@ -29,61 +14,49 @@ public class GridMaker : MonoBehaviour {
 
 	public float sizeRatioOfSmallBox = 0.8f;
 
-	public List<Transform> gridList;
+	//public List<Transform> gridList;
+	private ActiveSquareBehaviour[][] grid;
+	private Vector2 gridDimensions;
 
-	Vector3 zeroCord = new Vector3(0.0f, 0.0f, 0.0f);		//
+	Vector3 zeroCord = Vector3.zero;
 	Vector3 framingBoxSize;
 	Vector3 firstBoxCord;
 
-	public RaycastHit2D hit;
-	public Ray ray;
-	public Vector3 rayPosition;
-	//public bool mouseOnPlayArea;
-	public float mouseXpos;
-	public float mouseYpos;
-	public Vector3 junkV3;
-	public Vector2 origin;
 
-	public Transform sphere;
-	public Transform cube;
 
-	RaycastHit2D[] hitInfo;
 
 	void Start () {
-		//mouseOnPlayArea = false;
+		gridDimensions = new Vector2(boxCountX, boxCountY);
 
 		playAreaDetector.localScale = new Vector3(1.0f, 1.0f,1.0f);
 		playAreaDetector.position = gameObject.transform.position;
 
-		Transform smallSquareInst;
+		ActiveSquareBehaviour smallSquareInst;
 		transformOriginal = gameObject.transform;
 		framingBoxSize = new Vector3(1.0f/boxCountX, 1.0f/boxCountY, 1.0f);
 		firstBoxCord = zeroCord + new Vector3 ((-0.5f + framingBoxSize.x / 2), (0.5f - framingBoxSize.y / 2), 0.0f);
 		int yi = 0;
 		int xi = 0;
-		for (int i = 0; i < boxCountX*boxCountY; i++) {
-			smallSquareInst = Instantiate (smallSquare, zeroCord, transformOriginal.rotation);
-			smallSquareInst.transform.SetParent (gameObject.transform);
-			smallSquareInst.transform.localScale = framingBoxSize * sizeRatioOfSmallBox;
-			smallSquareInst.transform.localPosition = firstBoxCord + new Vector3(framingBoxSize.x*xi, -framingBoxSize.y*yi, -0.1f);
-			if (xi >= boxCountX-1) {
-				xi = 0;
-				yi++;
-			} else {
-				xi++;
+		grid = new ActiveSquareBehaviour[(int)gridDimensions.x][];
+		for (int x = 0; x < gridDimensions.x; x++){
+			grid[x] = new ActiveSquareBehaviour[(int)gridDimensions.y];
+			for (int y = 0; y < gridDimensions.y; y++)
+			{
+				smallSquareInst = Instantiate (smallSquare, zeroCord, transformOriginal.rotation);
+				smallSquareInst.transform.SetParent (gameObject.transform);
+				smallSquareInst.transform.localScale = framingBoxSize * sizeRatioOfSmallBox;
+				smallSquareInst.transform.localPosition = firstBoxCord + new Vector3(framingBoxSize.x*x, -framingBoxSize.y*y, -0.1f);
+				smallSquareInst.GridCordX = x;
+				smallSquareInst.GridCordY = y;
+				Debug.Log (smallSquareInst.GridCordX);
+				Debug.Log (smallSquareInst.GridCordX);
+				grid[x][y] = smallSquareInst;
+				Debug.Log (x);
+				Debug.Log (y);
 			}
-			gridList.Add (smallSquareInst);
 		}
 	}
-
-//	void hoverOverHighlight(){
-//		RaycastHit hit2;
-//		Ray ray2 = Camera.main.ScreenPointToRay(Input.mousePosition);
-//		if (Physics.Raycast(ray, out hit2)){
-//			rayHit.collider.enabled = false;
-//			Debug.Log ("raycasted");
-//		}
-//	}
+		
 //	void OnMouseEnter(){
 //		mouseOnPlayArea = true;
 //		Debug.Log ("gridmaker mouseonplayarea true");
@@ -94,66 +67,5 @@ public class GridMaker : MonoBehaviour {
 //	}
 	void Update () {
 		
-		if (Input.GetMouseButtonDown(0))
-		{
-			float rayDistance = 20.0f;
-			Vector3 mouseWorldPosition = Camera.main.transform.position;
-			Vector3 screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
-			Vector3 mouseWorldPosition2 = Camera.main.ScreenToWorldPoint(new Vector3((Input.mousePosition).x, (Input.mousePosition).y, screenPoint.z));
-			Ray ray = new Ray (mouseWorldPosition2, Vector3.zero);
-				//Debug.Log ("cast");
-			hitInfo = Physics2D.GetRayIntersectionAll(ray, rayDistance);
-			Debug.Log (hitInfo.Length);
-			for(int i =0; hitInfo.Length > i; i++){
-				Debug.Log (hitInfo[i].collider.gameObject.name);
-			}
-			
-			//origin = new Vector2(junkV3.x, junkV3.y);
-			//if (Physics2D.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), Vector2.zero)){
-			//Debug.Log(hitInfo.transform.name);
-			//}
-
-
-			sphere.position = mouseWorldPosition;
-			cube.position = mouseWorldPosition2;
-		}
-//		mouseXpos = Input.mousePosition.x;
-//		mouseYpos = Input.mousePosition.y;
-//		mouseXposAC = (new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0.0f));
-//		rayPosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane));
-//		//rayPosition = new Vector3 ((Input.mousePosition).x, (Input.mousePosition).y, 0.0f);
-//		ray = new Ray (rayPosition, Vector3.zero);
-//		hit = Physics2D.GetRayIntersection(ray, 0.0f);
-//		if (hit) {
-//			Debug.Log (hit.transform.name);
-//		}
-
-//		mouseXpos = Input.mousePosition.x;
-//		mouseYpos = Input.mousePosition.y;
-//		mouseXposAC = (new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0.0f));
-//		mouseYposAC = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0.0f));
-//		rayPosition = new Vector3 ((Input.mousePosition).x, (Input.mousePosition).y, 0.0f);
-//		ray = new Ray (rayPosition, new Vector3(5.0f, 5.0f, 5.0f));
-//		hit = Physics2D.GetRayIntersection(ray, 20.0f);
-//		if (hit) {
-//			Debug.Log (hit.transform.name);
-//		}
-
-//		//ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-//		if (Physics2D.Raycast (ray, out hit)) {
-//			if (hit.transform.tag == "Card") {
-//				//hit.transform. = false;
-//				Debug.Log ("raycasted");
-//			}
-//			Debug.Log (hit.transform.tag);
-//		}
-//		if 
-//		ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-//			RaycastHit hit2;
-//			Ray ray2 = Camera.main.ScreenPointToRay(Input.mousePosition);
-//			if (Physics.Raycast(ray, out hit2)){
-//				rayHit.collider.enabled = false;
-//				Debug.Log ("raycasted");
-//			}
 	}
 }
